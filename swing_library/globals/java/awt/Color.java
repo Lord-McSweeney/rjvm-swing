608ff97a9d1d411d6e85ecefc5f499c003c360fe
Lord-McSweeney <Lord-McSweeney@github.com>
2025-05-06 21:47:54 -0700

    swing_library: Add more Color constants

--- swing_library/globals/java/awt/Color.java
+++ swing_library/globals/java/awt/Color.java
@@ -7,6 +7,15 @@ public class Color {
     public static final Color LIGHT_GRAY = new Color(192, 192, 192);
     public static final Color WHITE = new Color(255, 255, 255);
 
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color CYAN = new Color(0, 255, 255);
+    public static final Color GREEN = new Color(0, 255, 0);
+    public static final Color MAGENTA = new Color(255, 0, 255);
+    public static final Color ORANGE = new Color(255, 200, 0);
+    public static final Color PINK = new Color(255, 175, 175);
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color YELLOW = new Color(255, 255, 0);
+
     private int r;
     private int g;
     private int b;
